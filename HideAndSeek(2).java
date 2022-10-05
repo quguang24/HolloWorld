24a6fa097c2e03bd712b7d68a09519156fef554c
DESKTOP-64JKUM6\曲广 <quguang24@gmail.com>
2022-10-05 20:19:58 +0900

    Programming is…

---
+++ HideAndSeek(2).java
@@ -0,0 +1,10 @@
+public class HideAndSeek {
+	public static void main(String[] ang) {
+		 // Press enter or return on your keyboard after each semicolon!
+		
+		System.out.println("Java is a class-based language.");
+	    System.out.println("Java classes have a 'main' method.");
+	    System.out.println("Java statements end with a semicolon.");
+	    System.out.println("Programming is… ");
+	}
+}
