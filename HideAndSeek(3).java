64ba4dff035f447ec1d70917b13c614664593115
DESKTOP-64JKUM6\曲广 <quguang24@gmail.com>
2022-10-05 21:54:41 +0900

    种树

---
+++ HideAndSeek(3).java
@@ -0,0 +1,31 @@
+public class HideAndSeek {
+  public static void main(String[] args) {
+ // Define your tree class in this file!
+ // I am a single line comment
+		 
+ /* It would appear that I, humble though
+	I may be, have been chosen as the 
+	multi-line comment for this example */
+		 System.out.print("我要砍树了,");
+		 System.out.println("我要弄脏我的手.");
+		 System.out.println("  *  ");
+		 System.out.println(" *** ");
+		 System.out.println(" *** ");
+		 System.out.println("  *  ");
+	     System.out.println("  *  ");
+	     System.out.println("  *  ");
+	     System.out.println(" * ** * ");
+	     System.out.println(" ****** ");
+	     System.out.println("   **   ");
+	     System.out.println("   **   "); 
+	     System.out.println(" ~~~~~~ ");
+	     System.out.println("  ***** ");
+	     System.out.println("  ***** ");
+	     System.out.println("  ***** ");
+	     System.out.println("  ***** ");
+	     System.out.println("    *   ");
+	     System.out.println("    *   ");
+	     System.out.println("  ###### ");
+		       
+		  }
+		}
