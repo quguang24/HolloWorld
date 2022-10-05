3304eb36a7fd19bd6eb7c68aac3b03a38b708964
DESKTOP-64JKUM6\曲广 <quguang24@gmail.com>
2022-10-05 19:44:05 +0900

    我们来捉迷藏

---
+++ HideAndSeek.java
@@ -0,0 +1,10 @@
+public class HideAndSeek {
+	public static void main(String[] ang) {
+		System.out.println("我们来捉迷藏.");
+		System.out.print("3...");
+		System.out.print("2...");
+		System.out.println("1...");
+		System.out.println("准备好了吧, 我来啦!");
+		
+	}
+}
