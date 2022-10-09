d4c2228f584f0fe7063024fd8b29406b4bad9c68
DESKTOP-64JKUM6\曲广 <quguang24@gmail.com>
2022-10-09 23:21:05 +0900

    故事

---
+++ 疯狂的故事.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+	 /*
+	  Your description here
+	  */
+	  	public static void main(String[] args){
+	      String name1 = "dapeng";
+	      String name2 = "xiaofeng";
+	      String adjective1 = "meilide";
+	      String adjective2 = "keaide";
+	      String adjective3 = "weiwude";
+	      String verb1 = "chifan";
+	      String noun1 ="feiji";
+	      String noun2 ="dapao";
+	      String noun3 ="tangke";
+	      String noun4 = "cidao";
+	      String noun5 = "qianting";
+	      String noun6 = "zhanjian";
+	      String place1 = "junquan";
+	      int number = 2100; 
+	      
+	      
+	      //The template for the story
+	      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+	      System.out.println(story);
+	    }       
+	}
+
+	    
+		
