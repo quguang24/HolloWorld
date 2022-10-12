83fbbc07e350145d271c26c8127de8ad353ce66c
DESKTOP-64JKUM6\曲广 <quguang24@gmail.com>
2022-10-12 20:57:50 +0900

    10.12

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] agrs) {
+		double[] nums = { 1.4, 1.5, 1.6, -1.4, -1.5, -1.6 };
+		for (double num : nums) {
+			test(num);
+		}
+    }
+
+	private static void test(double num) {
+		 System.out.println("Math.floor(" + num + ")=" + Math.floor(num));   
+		    System.out.println("Math.round(" + num + ")=" + Math.round(num));   
+		    System.out.println("Math.ceil(" + num + ")=" + Math.ceil(num));
+	}
+		
+	}
